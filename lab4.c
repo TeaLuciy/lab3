dde82bf0c1dfdb4dffd987fe2fcfc62698a5e83c
TeaLuciy <serega.mcraft@gmail.com>
2023-09-27 09:18:00 +0300

    refactored

--- lab4.c
+++ lab4.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include <math.h>
-double aproximate(double t)
-{
-    
-}
+
+
 int nextfib(int f1, int f2)
 {
     return f1 + f2;
 }
 
-int fib[10] = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
-int main()
-{
-    int fib1 = 1, fib2 = 1, fib3 = 2;
-    double tochnost, new_relation, old_relation;
 
-    scanf("%lg", &tochnost);
-
+double approximate(double t)
+{   
+    int fib1, fib2, fib3;
+    double new_relation, old_relation;
+    fib1=1;
+    fib2=1;
+    fib3=2;
     do
     {
         old_relation = 1.0 * fib2 / fib1;
@@ -25,7 +23,17 @@ int main()
         fib1 = fib2;
         fib2 = fib3;
         fib3 = nextfib(fib1, fib2);
-    } while (fabs(old_relation - new_relation) > tochnost);
+    } while (fabs(old_relation - new_relation) > t);
 
-    printf("%g\nAbsolute=%.10lf\n", new_relation, ((1+sqrt(5))/2));
+    return new_relation;
+
+}
+
+int main()
+{
+    double tochnost, new_relation, old_relation;
+
+    scanf("%lg", &tochnost);
+
+    printf("%.10g\nAbsolute=%.10lf\n", approximate(tochnost), ((1+sqrt(5))/2));
 }
