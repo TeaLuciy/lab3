fff7294bf046c774fb9c2ed50bc274a202943e1c
TeaLuciy <serega.mcraft@gmail.com>
2023-09-27 19:39:51 +0300

    2

--- lab4.c
+++ lab4.c
@@ -7,14 +7,9 @@ int nextfib(int f1, int f2)
     return f1 + f2;
 }
 
-double new_relation(int fib2, int fib3)
+double relation(int fib2, int fib3)
 {
     return 1.0 * fib3 / fib2;
-}
-
-double old_relation(int fib1, int fib2)
-{
-    return 1.0 * fib2 / fib1;
 }
 
 
@@ -29,19 +24,20 @@ double approximate(double accuracy)
         fib1 = fib2;
         fib2 = fib3;
         fib3 = nextfib(fib1, fib2);
-    } while (fabs(old_relation(fib1, fib2) - new_relation(fib2, fib3)) > accuracy);
+    } while (fabs(relation(fib1, fib2) - relation(fib2, fib3)) > accuracy);
 
-    return new_relation(fib2, fib3);
+    return relation(fib2, fib3);
 
 }
 
 int main()
 {
     double accuracy, golden_ratio, appr;
-    printf("tochnost=");
+    printf("accuracy=");
     scanf("%lg", &accuracy);
     golden_ratio = ((1+sqrt(5))/2);  // formula 
     appr = approximate(accuracy);
     printf("%.10g\nAbsolute=%.10lf\n", appr, golden_ratio);
-    printf("pogreshnost=%g\n", golden_ratio - appr);
+    printf("absolute error=%g\n", fabs(golden_ratio - appr));
+    printf("relative error=%g\n", fabs(golden_ratio - appr) / appr);
 }
