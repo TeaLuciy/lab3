4b62fc46552f5bce496cc94a08d22398f20e850b
TeaLuciy <serega.mcraft@gmail.com>
2023-09-28 23:30:32 +0300

    final

--- lab4.c
+++ lab4.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <math.h>
-
+double input(double a)
+{
+    while ((scanf("%lf", &a) != 1) || (a<=0)) // проверка ввода
+    {
+        printf("vvedite snova\n");
+        int c;
+        while ((c = getchar()) != '\n')
+        {
+        }
+    }
+    return a;
+}
 int nextfib(int f1, int f2)
 {
     return f1 + f2;
 }
 
-double relation(int fib2, int fib3)
+double relation(int fib1, int fib2)
 {
-    return 1.0 * fib3 / fib2;
+    return 1.0 * fib2 / fib1;
 }
 
 double approximate(double accuracy)
@@ -17,8 +28,10 @@ double approximate(double accuracy)
     fib1 = 1;
     fib2 = 1;
     fib3 = 2;
+    
     do
     {
+        printf("%d, %d, %d\n", fib1, fib2, fib3);
         fib1 = fib2;
         fib2 = fib3;
         fib3 = nextfib(fib1, fib2);
@@ -31,10 +44,11 @@ int main()
 {
     double accuracy, golden_ratio, appr;
     printf("accuracy=");
-    scanf("%lg", &accuracy);
+    accuracy = input(accuracy);
+    // scanf("%lg", &accuracy);
     golden_ratio = ((1 + sqrt(5)) / 2); // formula
     appr = approximate(accuracy);
-    printf("%.10g\nAbsolute=%.10lf\n", appr, golden_ratio);
+    printf("%g\nAbsolute=%.10lf\n", appr, golden_ratio);
     printf("absolute error=%g\n", fabs(golden_ratio - appr));
     printf("relative error=%g\n", fabs(golden_ratio - appr) / appr);
 }
