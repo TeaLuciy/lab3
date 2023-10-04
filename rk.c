ab6420371784e93dd2cb3657118def5a17dd4279
TeaLuciy <serega.mcraft@gmail.com>
2023-10-04 22:53:24 +0300

    predefend

--- rk.c
+++ rk.c
@@ -1,34 +1,40 @@
 #include <stdio.h>
 
-long long  input()
+
+unsigned long long  input()
 {
-    long long  new_num;
-    while ((scanf("%lld", &new_num) != 1) || (new_num<=0)) // проверка ввода
+    unsigned long long  new_num;
+    while ((scanf("%llu", &new_num) != 1) || (new_num<=0) || (new_num > 0x6DF37F67)) // проверка ввода
     {
-        printf("формат не верен\nввeдитe снова\n");
-        int c;
-        while ((c = getchar()) != '\n');
+        printf("format is not correct\nenter again\n");
+        while ((getchar()) != '\n');
     }
     return new_num;
 }
 
-long long newbase( long long base)
+unsigned long long newbase(unsigned long long base)
 {
-    long long count;
+    unsigned long long count;
     count = base;  
         while(count != 0)   // удвоенние колличества знаков
     {
         count=count/10;    
         base=base*10; 
     }
+    return base;
 }
 
 int main()
 {
-     long long base, result;
+    unsigned long long base, result;
     base = input();
-    //printf("size=%d; %lld\n", sizeof(base), base);
+    //printf("size=%d; %ull\n", sizeof(base), base);
     result = base;
     result += newbase(base);  
-    printf("ответ: %lld\n", result);
+    printf("answer: %llu\n", result);
 }
+
+
+//18446744073709551615   =  ffffffff
+//18446744071844674407
+//1844674407             =  6DF37F67      максимальное число которое можно удвоить
