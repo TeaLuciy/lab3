c44db1f3aef5cd58e52c2413340bb4711c18bf52
TeaLuciy <serega.mcraft@gmail.com>
2023-10-12 16:21:02 +0300

    final?

--- lab5.c
+++ lab5.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int  input()
+int input()
 {
-    int  new_num;
-    while ((scanf("%d", &new_num) != 1) || (new_num<=0) || (new_num > 100)) // проверка ввода
+    float new_num;
+    while ((scanf("%f", &new_num) != 1) || (new_num <= 0) || (new_num > 100)) // проверка ввода
     {
         printf("format is not correct\nenter again\n");
         while ((getchar()) != '\n');
@@ -12,10 +12,11 @@ int  input()
     return new_num;
 }
 
-
-void ggl(int age)  // год, года или лет
+void ggl(int age) // год, года или лет
 {
-        if ((age % 10 == 1) && (age % 100 != 11))
+
+    /*
+    if ((age % 10 == 1) && (age % 100 != 11))
     {
         printf("%d год", age);
     }
@@ -27,60 +28,96 @@ void ggl(int age)  // год, года или лет
     {
         printf("%d лет", age);
     }
+    */
+
+    switch (age)
+    {
+        case 11:
+        case 12:
+        case 13:
+        case 14:
+            printf("%d лет\n", age);
+            break;
+        default :
+            switch (age % 10)
+            {
+            case 1:
+                printf("%d год\n", age);
+                break;
+            case 2:
+            case 3:
+            case 4:
+                printf("%d года\n", age);
+                break;
+            default:
+                printf("%d лет\n", age);
+                break;
+            }
+    }
 }
 
 void sover(int age) // функция отвещающие за совершеннолетие
 {
     int sov;
-    sov= 18 - age;
+    sov = 18 - age;
     if (sov == 0)
     {
-        printf("Вы совершеннолетний\n");
+        printf("Вы совершеннолетний");
     }
     else if (sov > 0)
     {
         printf("Вам до совершеннолетия: ");
         ggl(sov);
-        printf("\n");
     }
-    else if (sov <0)
+    else if (sov < 0)
     {
         printf("Вы совершеннолетний уже ");
         ggl(abs(sov));
-        printf("\n");
     }
+    printf("\n");
 }
 
-void pens(int age)  // функция отвечающая за пенсию
+void pens(int age) // функция отвечающая за пенсию
 {
     int pen;
-    pen= 60 - age;
+    pen = 60 - age;
     if (pen == 0)
     {
-        printf("Вы пенсионер\n");
+        printf("Вы пенсионер");
     }
     else if (pen > 0)
     {
         printf("Вам до пенсии: ");
         ggl(pen);
-        printf("\n");
     }
-    else if (pen <0)
+    else if (pen < 0)
     {
         printf("Вы на пенсии ");
         ggl(abs(pen));
-        printf("\n");
     }
+    printf("\n");
 }
 
 int main()
 {
 
     int age;
+    printf("Введите ваш возраст: ");
     age = input();
-    printf("Ваш возраст:");
+    printf("Ваш возраст: ");
     ggl(age);
     printf("\n");
     sover(age);
     pens(age);
+
+
+    /*
+    for (size_t i = 0; i < 100; i++)
+    {
+        ggl(i);
+        printf(", ");
+    }
+    */
+    
+    
 }
