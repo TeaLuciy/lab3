9b5cd84c912ec64d51f7c4af04700d269663a1e8
TeaLuciy <serega.mcraft@gmail.com>
2023-10-17 21:33:54 +0300

    work?

--- lab6.c
+++ lab6.c
@@ -24,77 +24,82 @@ int inputss()
     return new_num;
 }
 
-char digit(int n)
+char digit(int n)     // переделать через sprintf
 {
-    switch (n)
-    {
-    case 0:
-        return '0';
-    case 1:
-        return '1';
-    case 2:
-        return '2';
-    case 3:
-        return '3';
-    case 4:
-        return '4';
-    case 5:
-        return '5';
-    case 6:
-        return '6';
-    case 7:
-        return '7';
-    case 8:
-        return '8';
-    case 9:
-        return '9';
-    case 10:
-        return 'a';
-    case 11:
-        return 'b';
-    case 12:
-        return 'c';
-    case 13:
-        return 'd';
-    case 14:
-        return 'e';
-    case 15:
-        return 'f';
-    default:
-        return '*';
-    }
+    char sixt[2];
+    int x=1;
+    snprintf(sixt, sizeof(sixt), "%X", n);
+
+    return sixt[0];
 }
 
 #define BUFF_SIZE 1024
 
 int main()
 {
+
+
     double num, drobn;
     int cel;
     int ss;
     char celss[BUFF_SIZE];
-    char *mid, *curr;
+    char *mid, *curr, *currdrob;
 
     memset(celss, 0, sizeof(celss));
     mid = celss + BUFF_SIZE/2;
     curr = mid;
+    currdrob = mid + 1;
+
 
     num=input();
     cel = (int)num;
     drobn = (num - cel);
     printf("%d %g\n", cel, drobn );
 
+
+
+    double ost;
     ss=inputss();
     while (cel > 0)
     {
-        int ost=cel%ss;
+        ost=cel%ss;
         curr--;
         *curr = digit(ost);
         cel=cel/ss;
     }
     printf("%s\n", curr);
+    
 
 
+
+    *mid = '.';
+
+
+
+    ost = 1;
+    int count =0;
+    while ( ost != 0)
+    {
+        if (count == 10)
+        {
+            mid++;
+            *mid = '.';
+            mid++;
+            *mid = '.';
+            break;
+        }
+        count++;
+        ost = drobn*ss;
+        mid++;
+        if (ost!=0)
+        {
+            *mid = digit(ost);
+        } else {
+            *mid = '0';
+        }
+        drobn = ost - (int)ost;
+    }
+    printf("%s\n", curr);
     return 0;
 
 }
