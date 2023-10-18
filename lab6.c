0c23bf5aca18b4585a51b8d00d068cb6551835a7
TeaLuciy <serega.mcraft@gmail.com>
2023-10-18 11:13:13 +0300

    refactored

--- lab6.c
+++ lab6.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#define BUFF_SIZE 1024
+#define TOCHN 10
 
-double input()
+double input() // ввод переводимого числа
 {
     double new_num;
     while ((scanf("%lf", &new_num) != 1) ) // проверка ввода
@@ -13,7 +16,7 @@ double input()
     return new_num;
 }
 
-int inputss()
+int inputss()  // ввод сс
 {
     int new_num;
     while ((scanf("%d", &new_num) != 1) || (new_num < 2) || (new_num > 16) ) // проверка ввода
@@ -24,7 +27,7 @@ int inputss()
     return new_num;
 }
 
-char digit(int n)     // переделать через sprintf
+char digit(int n)   //перевод цифры в сс
 {
     char sixt[2];
     int x=1;
@@ -33,33 +36,10 @@ char digit(int n)     // переделать через sprintf
     return sixt[0];
 }
 
-#define BUFF_SIZE 1024
-
-int main()
+void perevodcel(int cel, char *curr, int ss, int sign)  // перевод целой части в сс
 {
-
-
-    double num, drobn;
-    int cel;
-    int ss;
-    char celss[BUFF_SIZE];
-    char *mid, *curr, *currdrob;
-
-    memset(celss, 0, sizeof(celss));
-    mid = celss + BUFF_SIZE/2;
-    curr = mid;
-    currdrob = mid + 1;
-
-
-    num=input();
-    cel = (int)num;
-    drobn = (num - cel);
-    printf("%d %g\n", cel, drobn );
-
-
-
     double ost;
-    ss=inputss();
+
     while (cel > 0)
     {
         ost=cel%ss;
@@ -67,39 +47,83 @@ int main()
         *curr = digit(ost);
         cel=cel/ss;
     }
-    printf("%s\n", curr);
+    if (sign == -1) // добавление знака
+    {
+        curr--;
+        *curr='-';
+    }
     
+    printf("%s", curr);
 
+}
 
-
-    *mid = '.';
-
-
-
-    ost = 1;
+void perevoddrobn(double drobn, char *mid, int ss) // перевод дробной части в сс
+{
+    *mid='.';
+    char *currdrob = mid;
+    float ost = 1;
     int count =0;
     while ( ost != 0)
     {
-        if (count == 10)
+        if (count == TOCHN)
         {
-            mid++;
-            *mid = '.';
-            mid++;
-            *mid = '.';
+            currdrob++;
+            *currdrob = '.';
+            currdrob++;
+            *currdrob = '.';
             break;
         }
         count++;
         ost = drobn*ss;
-        mid++;
+        currdrob++;
         if (ost!=0)
         {
-            *mid = digit(ost);
-        } else {
-            *mid = '0';
+            *currdrob = digit(ost);
         }
         drobn = ost - (int)ost;
     }
-    printf("%s\n", curr);
+    printf("%s", mid);
+
+}
+
+void perevod(double num, int ss)
+{
+    double drobn;
+    int sign,cel;
+    char celss[BUFF_SIZE];
+    char *mid, *curr, *currdrob;
+    memset(celss, 0, sizeof(celss)); // заполнение строчки нулями
+    mid = celss + BUFF_SIZE/2; // середина строчки celss
+    curr = mid;
+    currdrob = mid + 1; 
+    sign = copysignf(1, num); // знак числа
+    num = fabs(num);
+    cel = (int)num;  // целая часть числа
+    drobn = (num - cel); // дробная часть числа
+    perevodcel(cel, curr, ss, sign);
+
+    if (drobn !=0)
+    {
+        perevoddrobn(drobn, mid++, ss);
+    }
+    
+
+}
+
+
+
+int main()
+{
+    double num;
+    int ss;
+
+
+
+
+
+    num=input();
+    ss=inputss();
+    perevod(num, ss);
     return 0;
 
 }
